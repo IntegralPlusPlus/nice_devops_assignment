931d87a0840bd66a58fb0e695b53645dfa456af3
IntegralPlusPlus <integralplusc@gmail.com>
2026-08-07 17:26:50 +0300

    feat(s3): add creation of encrypted S3 bucket

--- nice_assignment/nice_assignment_stack.py
+++ nice_assignment/nice_assignment_stack.py
@@ -1,19 +1,24 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    CfnOutput,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
 class NiceAssignmentStack(Stack):
+    # Serverless stack: S3 bucket + Lambda lister + SNS email notifications
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create S3 bucket
+        bucket = s3.Bucket(self, 
+                           "AssignmentBucket",
+                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                           encryption=s3.BucketEncryption.S3_MANAGED,
+                           removal_policy=RemovalPolicy.DESTROY,
+                           enforce_ssl=True,
+                           auto_delete_objects=True)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "NiceAssignmentQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        CfnOutput(self, "BucketName", value=bucket.bucket_name)
