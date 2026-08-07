26c3efcfc594a8bb1a323370b680901ced375dea
IntegralPlusPlus <integralplusc@gmail.com>
2026-08-07 20:58:02 +0300

    feat(sns): add SNS topic with email subscription

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ from nice_assignment.nice_assignment_stack import NiceAssignmentStack
 
 
 app = cdk.App()
+
 env = cdk.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"], 
                       region=os.environ["CDK_DEFAULT_REGION"])
 
-NiceAssignmentStack(app, "NiceAssignmentStack", env=env)
+# Get notification email
+notification_email = app.node.try_get_context("notification_email")
+
+NiceAssignmentStack(app, 
+                    "NiceAssignmentStack", 
+                    notification_email=notification_email,
+                    env=env)
 
 app.synth()
--- nice_assignment/nice_assignment_stack.py
+++ nice_assignment/nice_assignment_stack.py
@@ -3,14 +3,20 @@ from aws_cdk import (
     RemovalPolicy,
     CfnOutput,
     aws_s3 as s3,
-    aws_s3_deployment as s3deploy
+    aws_s3_deployment as s3deploy,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_ssm as ssm
 )
 from constructs import Construct
+
+# The path where we save the email in AWS SSM
+EMAIL_PARAM_NAME = "/nice-assignment/notification-email"
 
 class NiceAssignmentStack(Stack):
     # Serverless stack: S3 bucket + Lambda lister + SNS email notifications
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, notification_email: str | None = None, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create S3 bucket
@@ -31,5 +37,32 @@ class NiceAssignmentStack(Stack):
                                   retain_on_delete=False,
                                   prune=True
                                   )
-        
-        CfnOutput(self, "BucketName", value=bucket.bucket_name)
+
+        # Get the email from the command line (if provided), if not, download the saved email from AWS SSM
+        email = notification_email or ssm.StringParameter.value_from_lookup(self, EMAIL_PARAM_NAME)
+
+        # Save the email in AWS SSM securely, this helps us avoid writing real email addresses in the code
+        ssm.StringParameter(
+            self,
+            "NotificationEmailParam",
+            parameter_name=EMAIL_PARAM_NAME,
+            string_value=email,
+            description="Email for the SNS subscription; set on the first deployment",
+        )
+
+        # Create SNS topic
+        topic = sns.Topic(
+            self,
+            "NotificationTopic",
+            display_name="S3 Notifications",
+            topic_name="s3-notifications",
+        )
+
+        # Add subscription to SNS topic
+        topic.add_subscription(subs.EmailSubscription(email))
+
+        # Output bucket name
+        CfnOutput(self, "BucketName", value=bucket.bucket_name, description="S3 bucket")
+
+        # Output sns topic
+        CfnOutput(self, "TopicArn", value=topic.topic_arn, description="SNS topic")
