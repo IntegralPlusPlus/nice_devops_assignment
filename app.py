faf0e009a425296d131a7289d0873ef431a7368d
IntegralPlusPlus <integralplusc@gmail.com>
2026-08-07 16:40:13 +0300

    chore: set up CDK project

--- app.py
+++ app.py
@@ -7,22 +7,9 @@ from nice_assignment.nice_assignment_stack import NiceAssignmentStack
 
 
 app = cdk.App()
-NiceAssignmentStack(app, "NiceAssignmentStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+env = cdk.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"], 
+                      region=os.environ["CDK_DEFAULT_REGION"])
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+NiceAssignmentStack(app, "NiceAssignmentStack", env=env)
 
 app.synth()
